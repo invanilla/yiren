c8ca82ba050fb6b03595fc6996a7b17b535a12bd
white-violet <1481241918@qq.com>
2020-05-29 09:50:44 +0800

    更改

--- train.py
+++ train.py
@@ -6,15 +6,18 @@ from ddt import ddt,data
 class MyTestCase(unittest.TestCase):
 
     @data(1,2,3)
-    def test_something(self,value):
+    def test_00(self,value):
         self.assertEqual(value, 2)
 
+    @data(1,2,3)
+    def test_01(self,value):
+        self.assertEqual(value, 0)
 if __name__ == '__main__':
     # unittest.main(verbosity=2)
     loder = unittest.TestLoader()
     suite=loder.loadTestsFromTestCase(MyTestCase)
     #报告--H5报告
-    now = time.strftime("%Y%m%d%H%M%S", time.localtime())
-    with open("./report_%s.html" % now,"wb+") as fp:
+    # now = time.strftime("%Y%m%d%H%M%S", time.localtime())
+    with open("./report.html","wb+") as fp:
         runner=HTMLTestRunner.HTMLTestRunner(stream=fp,verbosity=3,title="测试报告")
         runner.run(suite)
